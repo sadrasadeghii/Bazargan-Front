72402b7c3a5c8a31726257cecafbe8c4de58d1c7
Ali chatraei <avida.chatraee2001@yahoo.com>
2022-08-04 20:46:17 +0430

    Add some config into tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
       container: {
         padding: "15rem",
       },
+      fontFamily: {
+        YekanBakh: ["YekanBakh", "Tahoma"],
+      },
+      spacing: {
+        "54px": "3.375rem",
+      },
+      colors: {
+        purple: "#3C096C",
+        gray: "#999999",
+      },
+      width: {
+        logo: "10rem",
+      },
     },
+    fontSize: {
+      title: "3rem",
+      caption: "2rem",
+      button: "1.625rem",
+    },
+
+    plugins: [],
   },
-  plugins: [],
 };
