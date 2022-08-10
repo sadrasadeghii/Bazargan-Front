05aca140362419107ed954f8dbdfd96e8ebe1d58
Hamed chatraei <hamed.chatraei@gmail.com>
2022-08-10 10:21:59 +0430

    Complete fullSlider

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-
+      fontFamily: {
+        'sans': ['YekanBakh', 'Helvetica', 'Arial', 'sans-serif']
+      }
     },
   },
   plugins: [],
